71534034b4d18dc4d96da094c2d3612de69c6b81
지원 <wi1767@naver.com>
2025-02-28 11:15:08 +0900

    🔧 리액트 패키지 구조 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        toss: "#4880EE",
+        background: "#F3F4F6",
+        extraButton: "#EAF2FE",
+        button: "#B2B8C0",
+        yuno: "#90CBFF",
+        black: "#1A1E27",
+        userBlack: "#414141",
+        red: "#DD5257",
+        placeholder: "#B2B8C0",
+      },
+    },
   },
   plugins: [],
 };
