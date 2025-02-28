d7fb9b646dd787646fcf8d647796d76c3f65d22a
katuman1234 <ktt5211@naver.com>
2025-02-28 11:02:02 +0900

    🔧 tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        toss: "#4880EE",
+        background: "#F3F4F6",
+        addbutton: "#EAF2FE",
+        button: "#B2B8C0",
+        charactor: "#B2B8C0",
+        font: "#1A1E27",
+        userFont: "#414141",
+        red: "#DD5257",
+        placeholder: "#B2B8C0",
+      },
+    },
   },
   plugins: [],
 };
