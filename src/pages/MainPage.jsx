e5cf302941f8cd877f84a3245368613153b05da2
지원 <wi1767@naver.com>
2025-02-28 15:41:56 +0900

    🔧 피드백 수정 - 속성들 작동 검사 + 전역속성 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const MainPage = () => {
-  return <div className="text-red-500 text-center font-PDBold">MainPage</div>;
+  return (
+    <div className="hover:bg-extraButton hover:ring-toss hover:ring-2 mx-auto my-10 w-width text-15 bg-background ring-2 ring-button text-red text-center font-PDBold rounded-15">
+      MainPage
+    </div>
+  );
 };
 
 export default MainPage;
