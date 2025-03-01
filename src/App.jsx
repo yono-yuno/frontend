c081e07d21912b9ccfedbde75c131f558717fe27
katuman1234 <ktt5211@naver.com>
2025-03-02 02:10:44 +0900

    🔧 Greeting 페이지 Route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./styles/style.css";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Ex from "./pages/Ex";
+import Greeting from "./pages/Greeting";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/Ex" element={<Ex />} />
+      <Route path="/Greeting" element={<Greeting />} />
     </Routes>
   );
 }
