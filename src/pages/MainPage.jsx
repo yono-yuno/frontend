3a3fc8970fa0f42017bae9136855487eb22e74f9
지원 <145041049+nowwizz@users.noreply.github.com>
2025-03-01 21:37:58 +0900

    🔧 헤더, 버튼 컴포넌트 추가 + tailwind.config에 헤더에 관련된 spacing 추가 + 파비콘 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 const MainPage = () => {
   return (
-    <div className="hover:bg-extraButton hover:ring-toss hover:ring-2 mx-auto my-10 w-width text-15 bg-background ring-2 ring-button text-red text-center font-PDBold rounded-15">
-      MainPage
-    </div>
+    <>
+      <Header text={"알림"} />
+      <div className="hover:bg-extraButton hover:ring-toss hover:ring-2 mx-auto my-10 w-width text-15 bg-background ring-2 ring-button text-red text-center font-PDBold rounded-15">
+        MainPage
+      </div>
+      <Button text={"확인"} />
+    </>
   );
 };
 
