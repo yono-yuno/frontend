d8f44c506daad5599e04fea967e5071d29fc137d
katuman1234 <ktt5211@naver.com>
2025-03-04 01:07:02 +0900

    💄 회원가입,Welcome 페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./styles/style.css";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import Ex from "./pages/Ex";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { LOGIN_PAGE_PATH, SIGNUP_PAGE_PATH } from "./constants/Paths";
 import WelcomPage from "./pages/WelcomPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/MainPage" element={<MainPage />} />
-      <Route path="/Ex" element={<Ex />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path={LOGIN_PAGE_PATH} element={<Login />} />
+      <Route path={SIGNUP_PAGE_PATH} element={<Signup />} />
       <Route path="/WelcomPage" element={<WelcomPage />} />
     </Routes>
   );
