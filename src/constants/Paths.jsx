2f95e51147070ec6071920b348a3027b491e7bfa
katuman1234 <ktt5211@naver.com>
2025-03-04 01:06:07 +0900

    💄 회원가입 페이지 퍼블리싱

---
+++ src/constants/Paths.jsx
@@ -0,0 +1,2 @@
+export const LOGIN_PAGE_PATH = "/login";
+export const SIGNUP_PAGE_PATH = "/signup";
---
+++ src/utils/FormatByAuth.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const FormatPhoneNumber = (value) => {
+  const onlyNums = value.replace(/[^0-9]/g, "");
+
+  if (onlyNums.length <= 3) {
+    return onlyNums;
+  }
+
+  if (onlyNums.length <= 7) {
+    return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3)}`;
+  }
+
+  // prettier-ignore
+  return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3, 7)}-${onlyNums.slice(7, 11)}`;
+};
+
+export const FormatPassWord = (value) => {
+  return value.replace(/[^0-9]/g, "").slice(0, 6);
+};
+
+export const FormatAccount = (value) => {
+  return value.replace(/[^0-9]/g, "").slice(0, 12);
+};
