81d0e1aee5f0a492723767b9122e05c8b1f0d96c
katuman1234 <ktt5211@naver.com>
2025-03-04 16:02:52 +0900

    🔧 초기설정 페이지 paths 경로 추가

--- src/constants/Paths.jsx
+++ src/constants/Paths.jsx
@@ -1,2 +1,5 @@
 export const LOGIN_PAGE_PATH = "/login";
 export const SIGNUP_PAGE_PATH = "/signup";
+export const WELCOME_PAGE_PATH = "/welcomePage";
+export const SETUP_PAGE_PATH = "/setupPage";
+export const SETUPCOMPLETE_PAGE_PATH = "/setupCompletePage";
