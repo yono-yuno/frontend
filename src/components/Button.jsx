08928e0b1fd55ab080b599b3e00feb6f24adbfc2
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-05 21:28:03 +0900

    💄 버튼 컴포넌트 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,8 @@ const Button = (props) => {
   return (
     <button
       onClick={props.onClick}
-      className="flex justify-center items-center mx-auto w-buttonWidth h-buttonHeight rounded-15 bg-toss font-PDMedium text-white text-20"
+      disabled={props.disabled}
+      className="flex justify-center items-center mx-auto w-buttonWidth h-buttonHeight rounded-15 bg-toss font-PDMedium text-white text-20 disabled:bg-background"
     >
       {props.text}
     </button>
