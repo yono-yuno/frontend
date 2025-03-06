7396b8e596d13738934864ce72f9d30d5a352c09
지원 <wi1767@naver.com>
2025-03-06 17:17:37 +0900

    💄 결제 페이지 퍼블리싱 완료 + 아이템 정보 컴포넌트 생성

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import ItemInfo from "../components/ItemInfo";
 import SmallAngryYuno from "../assets/SmallAngryYuno.png";
 import MediumAngryYuno from "../assets/MediumAngryYuno.png";
 import BigAngryYuno from "../assets/BigAngryYuno.png";
@@ -7,69 +9,214 @@ import SmallAngryBubble from "../assets/SmallAngryBubble.png";
 import MediumAngryBubble from "../assets/MediumAngryBubble.png";
 import BigAngryBubble from "../assets/BigAngryBubble.png";
 import ElectronicsCategoryIcon from "../assets/ElectronicsCategoryIcon.png";
+import TossAccountIcon from "../assets/TossAccountIcon.png";
+import TossPayIcon from "../assets/TossPayIcon.png";
+import Check from "../assets/Check.png";
+import SmallYuno from "../assets/SmallYuno.png";
+import { MAIN_PAGE_PATH } from "../constants/Paths";
 
 const PayPage = () => {
+  const navigate = useNavigate();
+
   const data = {
-    overprice: 500000,
-    status: 0,
+    overprice: 10000000,
+    balance: 1000000,
+    accountNum: 1234567890,
+    status: null,
     brandName: "토스 요노쇼핑",
     itemName: "Marshal WOBURN3 블루투스 스피커",
     price: 855000,
     itemImg: ElectronicsCategoryIcon,
+    WiseSaying:
+      '"소비는 나의 자유다. 하지만 그 자유는 선택에 달려 있다." — 로버트 키요사키',
   };
-  let yonoStatus = {};
+  let payStatus = {};
 
-  const Yuno = () => {
+  const Status = () => {
     switch (data.status) {
       default:
-        return (yonoStatus = {
+        return (payStatus = {
           bubble: SmallAngryBubble,
           talk: "흠.. 꼭 사야 될까요?",
+          talkColor: "black",
           yonoImg: SmallAngryYuno,
+          priceColor: "black",
+          bg: { background: "#F2F4F6" },
+          button1Text: "바로 결제",
+          button2Img: SmallYuno,
+          button2Text: "일단 고민해 보기",
         });
       case 0:
-        return (yonoStatus = {
+        return (payStatus = {
           bubble: MediumAngryBubble,
-          talk: "좀 더 고민해 볼까요??",
+          talk: "좀 더 고민해 볼까요?",
+          talkColor: "[#724141]",
           yonoImg: MediumAngryYuno,
+          priceColor: "lightRed",
+          bg: {
+            background: "linear-gradient(-180deg, #D8A9A9 5%, #F2F4F6 40%)",
+          },
+          button1Text: "바로 결제",
+          button2Img: SmallYuno,
+          button2Text: "더 고민해 보기",
         });
       case 1:
-        return (yonoStatus = {
+        return (payStatus = {
           bubble: BigAngryBubble,
           talk: "정말요?",
+          talkColor: "white",
           yonoImg: BigAngryYuno,
+          priceColor: "lightRed",
+          bg: {
+            background: "linear-gradient(-180deg, #918F8F 5%, #F2F4F6 40%)",
+          },
+          button1Text: "결제 취소",
+          button2Img: TossPayIcon,
+          button2Text: "Pay 결제하기",
         });
     }
   };
 
-  Yuno();
-
-  return (
-    <div className="w-full h-full bg-background">
-      <Header />
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center relative mt-[1px]">
-          <img src={yonoStatus.bubble} className="w-[188px] h-[68.5px]" />
-          <p className="absolute left-[45px] top-[14px] font-PDRegular text-16 text-black leading-tight">
-            {yonoStatus.talk}
+  const Button = () => {
+    return data.overprice > data.price ? (
+      <div className="flex flex-col items-center justify-center mt-[72px]">
+        <div className="flex items-center justify-center overflow-hidden mb-[6px] w-[319px] h-[32px] bg-extraButton rounded-[10px] font-PDMedium text-[13px] text-toss">
+          <p className="whitespace-nowrap animate-marquee leading-tight">
+            {data.WiseSaying}
           </p>
-          <img src={yonoStatus.yonoImg} className="w-[229px] h-[143px]" />
+          <p className="ml-[40px] whitespace-nowrap animate-marquee leading-tight">
+            {data.WiseSaying}
+          </p>
+          <p className="ml-[40px] whitespace-nowrap animate-marquee leading-tight">
+            {data.WiseSaying}
+          </p>
         </div>
-        <div className="flex items-center justify-center mt-[60px] w-width h-[147px] gap-[9px] bg-white rounded-15">
-          <img
-            src={data.itemImg}
-            className="w-[100px] h-[100px] border-[2px] border-background rounded-15 "
-          />
-          <div className="flex flex-col items-start justify-center gap-[8px] font-PDMedium text-16 leading-tight">
-            <p>{data.brandName}</p>
-            <p className="font-PDLight">{data.itemName}</p>
-            <p>
-              {data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원
+        <button className="flex flex-row items-center justify-center w-buttonWidth h-buttonHeight gap-[6px] rounded-15 bg-toss text-white">
+          <img src={TossPayIcon} className="w-[30px] h-[30px]" />
+          <p className="font-PDLight text-24">Pay</p>
+          <p className="font-PDLight text-20">동의하고 결제하기</p>
+        </button>
+        <div className="flex justify-center items-center gap-[5px] w-[300px] h-[31px] leading-tight">
+          <img src={Check} className="w-[17px] h-[17px] text-[13px]" />
+          <p className="font-PDSemibold text-toss">필수</p>
+          <p className="font-PDRegular text-[#999FA8]">
+            결제 정보 확인 및 정보 제공 동의
+          </p>
+        </div>
+      </div>
+    ) : (
+      <div className="flex flex-col items-center justify-center mt-[110px]">
+        <div className="flex flex-row items-center justify-center w-buttonWidth h-buttonHeight gap-[19px] font-PDLight text-20">
+          <button
+            className={`flex justify-center items-center w-[129px] h-buttonHeight rounded-15 ${
+              data.status == 1 ? "bg-lightRed" : "bg-extraButton"
+            } ${data.status == 1 ? "text-white" : "text-toss"}`}
+          >
+            <p>{payStatus.button1Text}</p>
+          </button>
+          <button
+            className={`flex justify-center items-center w-[203px] h-[59px] rounded-15 gap-[6px] bg-toss text-white`}
+          >
+            <img
+              src={payStatus.button2Img}
+              className={
+                data.status == 1 ? "w-[30px] h-[30px]" : "w-[23px] h-[27px]"
+              }
+            />
+            <p>{payStatus.button2Text}</p>
+          </button>
+        </div>
+        {data.status == 1 ? (
+          <div className="flex justify-center items-center gap-[5px] w-[300px] h-[31px] leading-tight">
+            <img src={Check} className="w-[17px] h-[17px] text-[13px]" />
+            <p className="font-PDSemibold text-toss">필수</p>
+
+            <p className="font-PDRegular text-[#999FA8]">
+              결제 정보 확인 및 정보 제공 동의
             </p>
           </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  };
+
+  const NavigateMain = () => {
+    navigate(MAIN_PAGE_PATH);
+  };
+  const NavigateShop = () => {};
+
+  Status();
+
+  return (
+    <div className="w-full h-full" style={payStatus.bg}>
+      <Header onClick={data.status == null ? NavigateShop : NavigateMain} />
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center relative mt-[1px]">
+          <img src={payStatus.bubble} className="w-[188px] h-[68.5px]" />
+          <p
+            className={`absolute left-[45px] top-[14px] font-PDRegular text-16 text-${payStatus.talkColor} leading-tight`}
+          >
+            {payStatus.talk}
+          </p>
+          <img src={payStatus.yonoImg} className="w-[229px] h-[143px]" />
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex items-center justify-center mt-[60px] w-width h-[147px] rounded-15 bg-white">
+          <ItemInfo
+            itemImg={data.itemImg}
+            brandName={data.brandName}
+            itemName={data.itemName}
+            priceColor={payStatus.priceColor}
+            price={data.price}
+          />
+        </div>
+        <div className="flex items-center mt-[15px] w-width h-[99px] rounded-15 bg-white">
+          <div className="flex flex-col ml-[22px] w-[174px] h-[71px]">
+            <p className="font-PDSemibold text-16 text-[#7C838D] leading-tight">
+              결제수단
+            </p>
+            {data.status == 1 ? (
+              <div className="flex w-[314px] mt-[7px] gap-[13px]">
+                <img src={TossAccountIcon} className="w-[40px] h-[40px]" />
+                <div className="flex flex-col items-start justify-center leading-tight">
+                  <div className="flex flex-row items-center gap-[7px] text-[21px] font-PDSemibold text-black">
+                    <p className="line-through">
+                      {data.balance
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      원
+                    </p>
+                    <p className="text-16">→</p>
+                    <p className="text-lightRed">
+                      {(data.balance - data.price)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      원
+                    </p>
+                  </div>
+
+                  <p className="font-PDRegular text-12 text-[#7B838F]">
+                    토스뱅크 {data.accountNum}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <div className="flex mt-[7px] gap-[13px]">
+                <img src={TossAccountIcon} className="w-[40px] h-[40px]" />
+                <div className="flex flex-col items-start justify-center leading-tight">
+                  <p className="font-PDSemibold text-16 text-black">
+                    토스뱅크 통장
+                  </p>
+                  <p className="font-PDRegular text-12 text-[#7B838F]">
+                    토스뱅크 {data.accountNum}
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <Button />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         yuno: "#90CBFF",
         black: "#1A1E27",
         userBlack: "#414141",
+        lightRed: "#FC6767",
         red: "#DD5257",
         placeholder: "#B2B8C0",
       },
@@ -43,6 +44,15 @@ module.exports = {
         buttonHeight: "59px",
         Hwidth: "393px",
         Hheight: "50px",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
       },
     },
   },
