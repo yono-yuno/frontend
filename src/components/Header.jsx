e6b3b6c70442d8a0856aa5b19bd6c1dd8b727c69
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-06 09:30:47 +0900

    💄 Header BackButton 크기 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import BackButton from "../assets/BackButton.png";
 const Header = (props) => {
   return (
     <div className="relative flex justify-center items-end w-Hwidth h-Hheight">
-      <button onClick={props.onClick} className="absolute left-[15px]">
+      <button onClick={props.onClick} className="absolute left-[15px] w-11">
         <img src={BackButton} className="w-[12.5px] h-[22.5px]" />
       </button>
       {/* leading-tight 필수: 글씨의 위아래 공백을 줄여줌 */}
