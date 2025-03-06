23a7ad9f898dcd43691aea22f461a51b4bd10da3
katuman1234 <ktt5211@naver.com>
2025-03-06 15:17:18 +0900

    🎨 Paths.jsx 페이지명 수정

--- src/constants/Paths.jsx
+++ src/constants/Paths.jsx
@@ -1,5 +1,5 @@
 export const LOGIN_PAGE_PATH = "/login";
 export const SIGNUP_PAGE_PATH = "/signup";
-export const WELCOME_PAGE_PATH = "/welcomePage";
-export const SETUP_PAGE_PATH = "/setupPage";
-export const SETUPCOMPLETE_PAGE_PATH = "/setupCompletePage";
+export const WELCOME_PAGE_PATH = "/welcome";
+export const SETUP_PAGE_PATH = "/setup";
+export const SETUPCOMPLETE_PAGE_PATH = "/setupComplete";
